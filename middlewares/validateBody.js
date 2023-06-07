95f8acd8c1c2b194d18f5810462cc04d401827f9
Kateryna Ostrynska <105549205+Ostrynska@users.noreply.github.com>
2023-06-07 12:52:36 +0300

    Update validateBody.js

--- middlewares/validateBody.js
+++ middlewares/validateBody.js
@@ -12,28 +12,4 @@ const validateBody = schema => {
     return fn;
 }
 
-const updateValidateBody = schema =>
-{
-        const fn = (req, res, next)=> {
-        const { error } = schema.validate(req.body);
-        if (error) {
-            next(HttpError(400, 'Missing field'));
-        }
-        next()
-    }
-    return fn;
-};
-
-const updateValidateFavorite = schema =>
-{
-        const fn = (req, res, next)=> {
-        const { error } = schema.validate(req.body);
-        if (error) {
-            next(HttpError(400, 'Missing field favorite'));
-        }
-        next()
-    }
-    return fn;
-};
-
-module.exports = {validateBody, updateValidateBody, updateValidateFavorite};
+module.exports = validateBody;
